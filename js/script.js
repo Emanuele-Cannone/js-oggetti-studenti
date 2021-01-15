930b7740bdb57a1d9961fecd6ff9d7189ce46531
Emanuele <cannone.emanuele@gmail.com>
2021-01-15 14:43:30 +0100

    Init

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso il for in tutte le proprietà.
+// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
